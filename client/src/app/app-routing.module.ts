64cf4155cae59b449e500250e79a5dc183369200
kd2718 <korydonati@gmail.com>
2019-04-01 23:37:40 -0700

    first commit

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {LandingComponent} from './nav/landing/landing.component';
+
+import {NotFoundComponent} from './nav/not-found/not-found.component';
+
+const routes: Routes = [
+  {path: '', component: LandingComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './nav/landing/landing.component';
+import { NotFoundComponent } from './nav/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
